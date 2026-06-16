dc441f3c272b5b45b0f45f9a37f214181b74ce53
Cameron Foale <c.foale@federation.edu.au>
2026-06-16 14:13:13 +1000

    Remove JS-based hero scrolling

--- docs/_includes/js/scroll.js
+++
@@ -1,7 +0,0 @@
-const root = document.querySelector(':root');
-root.style.setProperty('--scroll', '0% 0%')
-window.addEventListener('scroll', (ev) => {
-    const bodyY = parseInt(window.scrollY, 10);
-
-    root.style.setProperty('--scroll', `0% ${-bodyY * 0.3}px`);
-})
